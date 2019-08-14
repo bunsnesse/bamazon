b9e286cd0d525f95b89e09e298ea0f0a2d4c5991
Emma <e.m.bunsness@gmail.com>
2019-08-13 23:16:22 -0600

    adding bamazonCustomer.js

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
-CREATE DATABASE Bamazon;
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
 
-USE Bamazon;
+USE bamazon;
 
 CREATE TABLE products(
 	item_id INT NOT NULL AUTO_INCREMENT,
@@ -13,8 +14,15 @@ CREATE TABLE products(
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES
-	('Pen','Office Supplies',229.99,896,0),
-	('Pen','Office Supplies',229.99,896,0),
-	('Pen','Office Supplies',229.99,896,0),
-	('Pen','Office Supplies',229.99,896,0),
-	('Pen','Office Supplies',229.99,896,0);
+	('Pen','Office Supplies',1.99,100),
+	('Post-It Notes','Office Supplies',2.99,90),
+	('Couch','Home Goods',350.00,80),
+	('Lamp','Home Goods',10.98,70),
+	('TV','Entertainment',179.00,60),
+    ('Radio','Entertainment',35.99,50),
+    ('Smart Phone','Entertainment',350.00,40),
+    ('Education','Books',18.00,30),
+    ('All the Light We Cannot See','Books',21.00,20),
+    ('1985','Books',12.00,10);
+
+
